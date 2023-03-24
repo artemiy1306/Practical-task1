82d5cbb62460bc4d0586922b9cccd309441611a4
artemiy <karelskiy@mail.ru>
2023-03-24 22:48:08 +0300

    made zad8

---
+++ zad8/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Clear(); 
+Console.WriteLine("Введите число "); 
+int number = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+while(a < number){
+      if(a % 2 == 0 ){
+      Console.Write(a + ", ");
+} 
+     a++;
+}
