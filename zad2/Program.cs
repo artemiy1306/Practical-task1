b1341781d7ed73d1e7e50d644e0b55ed65a605d0
artemiy <karelskiy@mail.ru>
2023-03-24 19:41:27 +0300

    made zad2

--- zad2/Program.cs
+++ zad2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+Console.Clear(); 
+Console.Write("Введите A: "); 
+int numberA = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Введите B: "); 
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberA == numberB){
+  Console.WriteLine("A равно B");
+}
+else
+  if(numberA > numberB){
+     Console.WriteLine("A больше B");
+     Console.WriteLine($"max = {numberA}");
+}  
+  else{
+    Console.WriteLine("A меньше B");
+     Console.WriteLine($"max = {numberB}");
+  }
+     
